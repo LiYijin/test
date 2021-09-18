ce2b479cdd93c4aac87208090d9eeb9628e709b6
chenyanfan <chenyanfan18@mails.ucas.ac.cn>
2021-09-19 00:52:30 +0800

    cmt

--- cuda_learning/sgemm/sgemm.cu
+++ cuda_learning/sgemm/sgemm.cu
@@ -22,7 +22,7 @@ void cpu_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
     }
   }
   double tLast = cpuSecond()-tStart;
-  printf("cpu:%.6f\n",tLast);
+  printf("cpu:%.6f\n",tLast*1000.0);
 }
 
 __global__ void sgemm_v0(const float *A, const float *B, float *C, int M, int N, int K)
@@ -36,18 +36,43 @@ __global__ void sgemm_v0(const float *A, const float *B, float *C, int M, int N,
     C[i*N+j] = sum;
   }
 }
-
+/*
+__global__ void sgemm_v1(const float *A, const float *B, float *C, int M, int N, int K)
+{
+  __shared__ mm[]
+  int j = blockIdx.x*blockDim.x + threadIdx.x;
+  int i = blockIdx.y*blockDim.y + threadIdx.y;
+  if(i<M && j<N){
+    float sum = 0;
+    for(int k=0;k<K;k++)
+      sum += A[i*K+k] * B[k*N+j];
+    C[i*N+j] = sum;
+  }
+}
+*/
 void gpu_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
 {
   dim3 block(BLOCKDIM,BLOCKDIM);
   dim3 grid((N-1)/block.x+1,(M-1)/block.y+1);
+  double tStart,tLast;
+  tStart = cpuSecond();
   sgemm_v0<<<grid,block>>>(A,B,C,M,N,K);
+  cudaDeviceSynchronize();
+  tLast = cpuSecond()-tStart;
+  printf("gpu:%.6f\n",tLast*1000.0);
+
+  tStart = cpuSecond();
+  sgemm_v1<<<grid,block>>>(A,B,C,M,N,K);
+  cudaDeviceSynchronize();
+  tLast = cpuSecond()-tStart;
+  printf("gpu:%.6f\n",tLast*1000.0);
 }
 void cublas_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
 {
   cublasHandle_t handle;
   float alpha=1, beta=0;
   CHECK_CUBLAS(cublasCreate(&handle));
+  double tStart = cpuSecond();
   //(BtAt)t = AB
   cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
       N, M, K, 
@@ -57,8 +82,10 @@ void cublas_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
       &beta, 
       C, N);
   cudaDeviceSynchronize();
+  double tLast = cpuSecond()-tStart;
   cublasDestroy(handle);
 }
+
 
 int main(int argc,char **argv)
 {
@@ -83,12 +110,14 @@ int main(int argc,char **argv)
   CHECK(cudaMemcpy(A_d,A,M*K*sizeof(float),cudaMemcpyHostToDevice));
   CHECK(cudaMemcpy(B_d,B,K*N*sizeof(float),cudaMemcpyHostToDevice));
 
-  //cpu_sgemm(A,B,C,M,N,K);
+  cpu_sgemm(A,B,C,M,N,K);
+
   gpu_sgemm(A_d,B_d,C_d,M,N,K);
   CHECK(cudaMemcpy(C,C_d,M*N*sizeof(float),cudaMemcpyDeviceToHost));
-  cublas_sgemm(A_d,B_d,C_d,M,N,K);
 
+  cublas_sgemm(A_d,B_d,C_d,M,N,K);
   CHECK(cudaMemcpy(C_ref,C_d,M*N*sizeof(float),cudaMemcpyDeviceToHost));
+  
   checkResult(C,C_ref,M*N);
 
   cudaFree(A_d);
