4200bc62f5ae4a2248d3f15e43a4a97270792689
chenyanfan <chenyanfan18@mails.ucas.ac.cn>
2021-09-27 00:03:46 +0800

    cmt

--- cuda_learning/sgemm/sgemm.cu
+++ cuda_learning/sgemm/sgemm.cu
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel1.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel2.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel3.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel4.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel5.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel6.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel7.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel8.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel9.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel10.cuh"
+#include "./Optimizing-SGEMM-on-NVIDIA-Turing-GPUs/include/kernel11.cuh"
 
 //A: M*K
 //B: K*N
@@ -51,11 +62,11 @@ __global__ void sgemm_v1(const float *A, const float *B, float *C, int M, int N,
     if(iy<M && j<K)
       mm1[threadIdx.y][threadIdx.x] = A[iy*K+j];
     if(j<K && ix<N)
-      mm2[threadIdx.y][threadIdx.x] = B[j*N+ix];
+      mm2[threadIdx.x][threadIdx.y] = B[j*N+ix];
     __syncthreads();
     #pragma unroll 4
     for(int k=0;k<BLOCKDIM && k+tileidx<K;k++)
-      sum += mm1[threadIdx.y][k] * mm2[k][threadIdx.x];
+      sum += mm1[threadIdx.y][k] * mm2[threadIdx.x][k];
     __syncthreads();
   }
   int i = blockIdx.y*blockDim.y+threadIdx.y;
@@ -108,23 +119,134 @@ __global__ void sgemm_v2(const float *A, const float *B, float *C, int M, int N,
     }
   }
 }
+#define FLOAT float
+#define INT int
+#define CEIL_DIV(m,n) ( (m) + (n) - 1 ) / (n)
 
-void gpu_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
+
+void test_mysgemm_v1(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(32,32);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v1<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v2(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(32,32);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v2<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v3(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(1024);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v3<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v4(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(1024);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v4<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v5(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v5<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+void test_mysgemm_v6(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,32),CEIL_DIV(N,32));
+    mysgemm_v6<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v7(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,64),CEIL_DIV(N,64));
+    mysgemm_v7<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v8(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,128),CEIL_DIV(N,128));
+    mysgemm_v8<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v9(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,128),CEIL_DIV(N,128));
+    mysgemm_v9<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v10(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,128),CEIL_DIV(N,128));
+    mysgemm_v10<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+void test_mysgemm_v11(INT M, INT N, INT K, FLOAT alpha, FLOAT *A, FLOAT *B, FLOAT beta, FLOAT *C){
+    cudaDeviceSynchronize();
+    dim3 blockDim(256);
+    dim3 gridDim(CEIL_DIV(M,128),CEIL_DIV(N,128));
+    mysgemm_v11<<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
+
+const int loop=500;
+void gpu_sgemm(float *A, float *B, float *C, int M, int N, int K)
 {
   dim3 block(BLOCKDIM,BLOCKDIM);
   dim3 grid((N-1)/block.x+1,(M-1)/block.y+1);
+  for(int i=0;i<loop;i++){
+    sgemm_v1<<<grid,block>>>(A,B,C,M,N,K);
+    cudaDeviceSynchronize();
+  }
+
   double tStart,tLast;
   tStart = cpuSecond();
-  sgemm_v0<<<grid,block>>>(A,B,C,M,N,K);
-  cudaDeviceSynchronize();
+  for(int i=0;i<loop;i++){
+    sgemm_v0<<<grid,block>>>(A,B,C,M,N,K);
+    cudaDeviceSynchronize();
+  }
   tLast = cpuSecond()-tStart;
-  printf("gpuv0:%.6f\n",tLast*1000.0);
+  printf("gpuv0:%.6f\n",tLast*1000.0/loop);
 
   tStart = cpuSecond();
-  sgemm_v2<<<grid,block>>>(A,B,C,M,N,K);
+  for(int i=0;i<loop;i++){
+    //test_mysgemm_v4(M,N,K,1,A,B,0,C);
+    sgemm_v1<<<grid,block>>>(A,B,C,M,N,K);
+    cudaDeviceSynchronize();
+  }
+  tLast = cpuSecond()-tStart;
+  printf("gpuv1:%.6f\n",tLast*1000.0/loop);
+/*
+  tStart = cpuSecond();
+  for(int i=0;i<loop;i++)
+    test_mysgemm_v3(M,N,K,1,A,B,0,C);
+  //mysgemm_v6<<<grid,block>>>(M, N, K, 1, A, B, 0, C);
   cudaDeviceSynchronize();
   tLast = cpuSecond()-tStart;
-  printf("gpuv1:%.6f\n",tLast*1000.0);
+  printf("gpuv2:%.6f\n",tLast*1000.0/loop);
+  */
 }
 void cublas_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
 {
@@ -133,16 +255,18 @@ void cublas_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
   CHECK_CUBLAS(cublasCreate(&handle));
   double tStart = cpuSecond();
   //(BtAt)t = AB
-  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
-      N, M, K, 
-      &alpha, 
-      B, N,
-      A, K, 
-      &beta, 
-      C, N);
-  cudaDeviceSynchronize();
+  for(int i=0;i<loop;i++){
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 
+        N, M, K, 
+        &alpha, 
+        B, N,
+        A, K, 
+        &beta, 
+        C, N);
+    cudaDeviceSynchronize();
+  }
   double tLast = cpuSecond()-tStart;
-  printf("cublas:%.6f\n",tLast*1000.0);
+  printf("cublas:%.6f\n",tLast*1000.0/loop);
   cublasDestroy(handle);
 }
 
@@ -150,7 +274,7 @@ void cublas_sgemm(const float *A, const float *B, float *C, int M, int N, int K)
 int main(int argc,char **argv)
 {
   float *A,*B,*C,*C_ref;
-  int M=1000,N=1024,K=1700;
+  int M=1024,N=1024,K=1024;
   if(argc==4){
     M = atoi(argv[1]);
     N = atoi(argv[2]);
